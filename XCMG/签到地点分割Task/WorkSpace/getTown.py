c0ef060505a507ecc0da2f6e30bd4fdf022713d1
jinzhaoPC <jzjinzhao@qq.com>
2025-07-08 23:09:19 +0800

    250708

--- XCMG/签到地点分割Task/WorkSpace/getTown.py
+++ XCMG/签到地点分割Task/WorkSpace/getTown.py
@@ -1,7 +1,6 @@
 # encoding:utf-8
 import requests 
-import json
-import ast
+import sys
 
 # 接口地址
 url = "https://api.map.baidu.com/place/v3/region"
@@ -9,21 +8,29 @@ url = "https://api.map.baidu.com/place/v3/region"
 # 此处填写你在控制台-应用管理-创建应用后获取的AK
 ak = "ZwZud9nuWcy3JB7olb2TLwB2syQPu13i"
 
+# 调用该Python脚本时传入参数： xx市 xxxxxx(地址信息)
+if len(sys.argv) != 3:
+    print('参数数目错误！应传参为： 【城市名】 【所查询地址信息】')
+    exit(1)
+
+regionParam = sys.argv[1]   # 获取xx市的参数
+queryParam = sys.argv[2]    # 获取地址信息的参数
+# print('regionParam: ' + regionParam)
+# print('queryParam: ' + queryParam)
+
 params = {
-    "query": "上海市闵行区新虹街道申昆路申虹国际大厦",
-    "region": "上海",
+    "query": queryParam,
+    "region": regionParam,
     "output": "json",
-    "is_light_version": False,
-    "address_result": False,
+    "is_light_version": "false",
+    "address_result": "false",
+    "scope": "2",
     "ak": ak,
 }
 
 response = requests.get(url=url, params=params)
 if response:
-    data = json.loads(response.json())
-    dataCleaned = data.replace("'", '"')
-    # data = ast.literal_eval(response.json())
-    firstResult = dataCleaned["results"][0]
-    townValue = firstResult["town"]
-    print(townValue)
-    # print(response.json().results[0].town)
+    dataDict = response.json()
+    # print(dataDict)
+    town = dataDict['results'][0]['town']
+    print(town)
